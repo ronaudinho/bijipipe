59fce74bfc9e5159d6a2c1cb99990ac4b1242ece
ronaudinho <doelaudi@gmail.com>
2019-01-21 15:07:29 +0700

    script to diff commit based on service

--- script/build.sh
+++ script/build.sh
@@ -1,2 +1,11 @@
 #!/bin/bash -e
-echo git --no-pager diff --name-only HEAD HEAD~1 | sort -u | awk 'BEGIN {FS="/"} {print $2}' | uniq
+DIFF="$(git --no-pager diff --name-only HEAD HEAD~1 | awk 'BEGIN {FS="/"} {print $2}' | sort -u)"
+if [ ${#DIFF[@]} == 0 ]; then
+	echo "sama aja biji"
+else
+	echo "beda bijinya:"
+	for i in "${DIFF[@]}"
+	do
+		echo "$i"
+	done
+fi
